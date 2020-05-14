5630107c608dcc94c35f96854603df1b1932f57d
rosanaemidio <rosanaemidio42@gmail.com>
2020-05-14 14:55:19 -0300

    Create column action

--- frontend/src/app/components/product/product-read2/product-read2.component.ts
+++ frontend/src/app/components/product/product-read2/product-read2.component.ts
@@ -17,7 +17,7 @@ export class ProductRead2Component implements AfterViewInit, OnInit {
   dataSource: ProductRead2DataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
   ngOnInit() {
     this.dataSource = new ProductRead2DataSource();
