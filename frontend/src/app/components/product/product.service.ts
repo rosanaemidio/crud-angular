5c4be1fc9d2f1b78d2375c9e362ff0550abce53f
rosanaemidio <rosanaemidio42@gmail.com>
2020-05-15 13:05:09 -0300

    Create component product delete and add updates in styles

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -30,7 +30,7 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  readById(id: string): Observable<Product> {
+  readById(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<Product>(url)
   }
@@ -39,4 +39,10 @@ export class ProductService {
     const url = `${this.baseUrl}/${product.id}`
     return this.http.put<Product>(url, product)
   }
+
+  delete(id: number): Observable<Product>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Product>(url)
+  }
+
 } 
